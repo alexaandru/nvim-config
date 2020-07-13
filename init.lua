669ec567df74e77be1e5f1e7d2ce0ad7aae769c1
Alex Ungur <alexaandru@gmail.com>
2020-07-13 10:48:32 +0300

    A little lua code cleanup

--- init.lua
+++ init.lua
@@ -11,20 +11,26 @@ require'nvim_lsp'.tsserver.setup{}
 require'nvim_lsp'.vimls.setup{}
 require'nvim_lsp'.vuels.setup{}
 
+local vim = vim
+
 -- Synchronously organise (Go) imports.
-function go_org_imports(timeout_ms)
-  local context = { source = { organizeImports = true } }
-  vim.validate { context = { context, 't', true } }
+-- Courtesy of https://github.com/neovim/nvim-lsp/issues/115
+function GoOrgImports(timeout_ms)
+  local context = {source={organizeImports=true}}
+  vim.validate {context={context, 't', true}}
+
   local params = vim.lsp.util.make_range_params()
   params.context = context
 
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
   if not result then return end
+
   result = result[1].result
   if not result then return end
-  edit = result[1].edit
+
+  local edit = result[1].edit
   vim.lsp.util.apply_workspace_edit(edit)
 end
 
-vim.api.nvim_command("au BufWritePre *.go lua go_org_imports(500)")
+vim.api.nvim_command("au BufWritePre *.go lua GoOrgImports(500)")
 vim.api.nvim_command("au BufWritePre *.go lua vim.lsp.buf.formatting_sync()")
